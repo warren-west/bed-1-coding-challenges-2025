db7a163b3246a63a458f19175417b0d674f280e8
warren-west <teacher.warren.english@gmail.com>
2025-09-26 12:23:53 +0200

    chore: Added some code comments to challenge 1

--- 01-sum-of-2-numbers.js
+++ 01-sum-of-2-numbers.js
@@ -1,8 +1,11 @@
+// A function that receives two numbers, and returns the sum of them
 function addition(num1, num2) {
+    // Return the sum of the two numbers
     return num1 + num2
 }
 
 
+// Test the addition function with different parameters
 console.log(addition(3, 2))
 console.log(addition(-3, -6))
 console.log(addition(7, 3))
