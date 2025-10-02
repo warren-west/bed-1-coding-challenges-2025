ba495f3954bfd45e2585ca9f246baf8fac2985b2
warren-west <teacher.warren.english@gmail.com>
2025-10-02 11:41:59 +0200

    feat: Added solution for 4th coding challenge

---
+++ 04-return-first-element-of-array.js
@@ -0,0 +1,11 @@
+const data1 = [5, 7, 3, 4]
+const data2 = ["Reverse", "in", "Words"]
+const data3 = [true, false, false, true, true]
+
+function getFirst(array) {
+    return array[0]
+}
+
+console.log(getFirst(data1))
+console.log(getFirst(data2))
+console.log(getFirst(data3))
